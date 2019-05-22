79413e389beab1057115819cc65d96b1ea135154
Gosha Maruzhenko <gmaruzhenko@yahoo.ca>
2019-05-21 21:35:15 -0700

    done A1

--- pressure_alltitude_plot.py
+++ pressure_alltitude_plot.py
@@ -18,7 +18,7 @@ MAX_HEIGHT = 14000
 dZ = 1   #m
 
 
-def inittilize_IC():
+def inittilize_ic():
     pressure_array[0] = START_PRESSURE
     temperature_array[0] = START_TEMP
 
@@ -30,23 +30,25 @@ def update_arrays(position):
     temperature_array[position + 1] = temperature_array[position] + dT
 
 
+def plot_charts():
+    plt.plot(hight_array, pressure_array)
+    # plt.plot(hight_array, temperature_array, 'red')
+    plt.title('Elevation vs Atmospheric Pressure')
+    plt.xlabel('Elevation above Sea Level (m)')
+    plt.ylabel('Pressure (kPa)')
+    plt.show()
+
+
 # Start code
 pressure_array = np.ones(int(MAX_HEIGHT/dZ))
 temperature_array = np.ones(int(MAX_HEIGHT/dZ))
 hight_array = [i * dZ for i in range(0, int(MAX_HEIGHT/dZ))]
 
-
-inittilize_IC()
+inittilize_ic()
 
 index = 0
 while index * dZ < MAX_HEIGHT - 1:
     update_arrays(index)
     index += 1
 
-plt.plot(hight_array, pressure_array)
-#plt.plot(hight_array, temperature_array, 'red')
-plt.title('Elevation vs Atmospheric Pressure')
-plt.xlabel('Elevation above Sea Level (m)')
-plt.ylabel('Pressure (kPa)')
-
-plt.show()
+plot_charts()
